70727fc07c73669ab202819421d84b99a4b0cde5
heather <crystal_tree@hotmail.com>
2018-01-09 15:02:39 -0600

    Contains Guess the number program and camelCase program

---
+++ Guess the number.py
@@ -0,0 +1,18 @@
+#Guess the number
+
+import random
+
+r= random.randint(1,11)
+
+guess=input("Guess a number between 1 and 10.")
+
+g=int(guess)
+print ("g1 "+str(g))
+while g != r:
+    if g > r:
+        g=int(input(str(g)+ " is too high.  Try again."))
+        print("g2 "+str(g))
+    elif g<r:
+        g = int(input(str(g) + " is too low.  Try again."))
+        print("g3 " + str(g))
+print ("Good job, you got the right answer!")
